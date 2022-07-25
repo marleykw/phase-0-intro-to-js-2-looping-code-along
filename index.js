8bdd33960b1cd40cefd080ad62a09df7d1280191
Marley Watson <marleykw17@gmail.com>
2022-07-25 10:25:38 -0400

    commit tree d498bd6625f3d53bb2ef2efd911b47ff027318e9

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const recipients = ['Guadalupe', 'Ollie', 'Aki'];
+console.log(recipients);
+console.log(recipients.length);
+const messages = [];
+function writeCards(recipients, occasion = 'surprise') {
+  for (let i = 0; i < recipients.length; i++) {
+    console.log(recipients[i]);
+    console.log(`Thank you, ${recipients[i]}, for the wonderful ${occasion} gift!`);
+    //const recipients = {
+    messages.push(`Thank you, ${recipients[i]}, for the wonderful ${occasion} gift!`); 
+    //}    // debugger;
+    console.log(messages);
+  }
+  return messages
+}
+
+function countDown(value) {
+  for (let i = 0; i <= value; i++) {
+    console.log(value - i);
+  }
+}
